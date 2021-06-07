f0c654742afcaed207c4a00662d7ca2054115e55
d-chechelashvili <dchec18@freeuni.edu.ge>
2021-06-07 14:20:42 +0400

    too hard to debug, starting over

--- simulator/dv_router.py
+++ simulator/dv_router.py
@@ -21,10 +21,7 @@ class DVRouter(basics.DVRouterBase):
 
         You probably want to do some additional initialization here.
         """
-        # port to latency mapping
-        self.ports = {}
-        # host to (destination, port, latency, receive_time)
-        self.table = {}
+        pass
         self.start_timer()  # Starts calling handle_timer() at correct rate
 
     def handle_link_up(self, port, latency):
@@ -33,12 +30,7 @@ class DVRouter(basics.DVRouterBase):
 
         The port attached to the link and the link latency are passed in.
         """
-        # add new link to port-weight dict
-        self.ports[port] = latency
-        # tell new link what I can reach with what costs
-        for host in self.table:
-            info = basics.RoutePacket(host, self.table[host][2])  # 2 is index of latency
-            self.send(info, port)
+        pass
 
     def handle_link_down(self, port):
         """
@@ -46,20 +38,7 @@ class DVRouter(basics.DVRouterBase):
 
         The port number used by the link is passed in.
         """
-        # remove port from our remembered ports
-        if port in self.ports:
-            self.ports.pop(port)
-
-        to_be_removed = []  # to not mutate collection while iterating
-        for host in self.table:  # for host in hosts
-            if port == self.table[host][1]:  # if I could reach that entity from this port
-                to_be_removed.append(host)  # remove that route from my table
-                if self.POISON_MODE:
-                    info = basics.RoutePacket(host, INFINITY)  # send that host poisoned route
-                    self.send(info, port, flood=True)  # with flood mode packets are sent from all ports except listed
-
-        for host in to_be_removed:
-            self.table.pop(host)
+        pass
 
     def handle_rx(self, packet, port):
         """
@@ -72,36 +51,12 @@ class DVRouter(basics.DVRouterBase):
         """
         self.log("RX %s on %s from %s trace: [%s]", packet, port, packet.src, ', '.join(map(str, packet.trace)))
         if isinstance(packet, basics.RoutePacket):
-            # if we dont have that destination in table
-            # or if we have it but new route has cheaper cost
-            # or if cost changed due to congestion or some other reason
-            # we should update our table
-            new_cost = self.ports[port] + packet.latency
-            dest = packet.destination
-            if dest in self.table and new_cost >= INFINITY and new_cost > self.table[dest][2]:
-                update_packet = basics.RoutePacket(dest, self.table[dest][2])
-                self.send(update_packet, flood=True)
-            elif dest not in self.table or new_cost <= self.table[dest][2] or self.table[dest][1] == port:
-                if new_cost < INFINITY:
-                    self.table[dest] = (packet.dst, port, new_cost, api.current_time())
-                update_packet = basics.RoutePacket(dest, new_cost)
-                self.send(update_packet, port, flood=True)
+            pass
         elif isinstance(packet, basics.HostDiscoveryPacket):
-            # if I don't know about the host or if the new way is cheaper I update the table
-            new_cost = self.ports[port]
-            src = packet.src
-            if src not in self.table or new_cost < self.table[src]:
-                self.table[packet.src] = (packet.dst, port, new_cost, api.current_time())
-                # tell other routers about update
-                update_packet = basics.RoutePacket(packet.src, new_cost)
-                self.send(update_packet, port, flood=True)
+            pass
         else:
-            # it is neither routePacket nor hostDiscover, lets just forward it
-            dest = packet.dst
-            if dest in self.table:
-                _, dest_port, dest_latency, _ = self.table[dest]
-                if dest_latency < INFINITY and port != dest_port:
-                    self.send(packet, dest_port)
+            pass
+
 
     def handle_timer(self):
         """
@@ -110,27 +65,6 @@ class DVRouter(basics.DVRouterBase):
         When called, your router should send tables to neighbors.  It also might
         not be a bad place to check for whether any entries have expired.
         """
-        self.remove_expired()
-        print self.name + ": " + str(self.table)
-        # action after expiring
-        for host in self.table:
-            # detect neighbor with following logic:
-            # if route to neighbor has same latency as one hop from that route's port then it is a neighbor
-            route_port = self.table[host][1]
-            info = basics.RoutePacket(host, self.table[host][2])  # 2 is index of latency
-            self.send(info, route_port, flood=True)
-            # if route_port in self.ports and self.ports[route_port] == self.table[host][2]:
-            #     self.send_table(route_port)
+        pass
 
-    def remove_expired(self):
-        expired = []  # to not mutate collection while iterating
-        for host in self.table:
-            if api.current_time() - self.table[host][3] > self.TIMEOUT:
-                expired.append(host)
-        for host in expired:
-            self.table.pop(host)
 
-    def send_table(self, route_port):
-        for host in self.table:
-            info = basics.RoutePacket(host, self.table[host][2])  # 2 is index of latency
-            self.send(info, route_port)
